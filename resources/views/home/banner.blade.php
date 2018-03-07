f47093854f52a3e6fe5b2b22be2623fa3b16b398
kream <kream8@outlook.com>
2018-03-07 16:59:49 +0800

    del some files

--- resources/views/home/banner.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="rslides_container">
-      <ul class="rslides" id="slider">
-      	@foreach($banners as $banner)
-        <li><a href="{{ route('show',$banner->id) }}" target="_blank" title="{{$banner->title}}"><img src="{{$banner->thumb}}" alt="{{$banner->title}}"/></a></li>
-		@endforeach
-      </ul>
-    </div>
